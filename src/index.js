392e3c2d93411134bcbf085bd954e82de704b947
yousejp <yousejp25@gmail.com>
2020-09-18 11:35:14 +0900

    init commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import './style.scss';
+
+ReactDOM.render(
+  <div>Hello, React!</div>,
+  document.getElementById('root'),
+);
