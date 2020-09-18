88bc767848d6815a8f8ec977af1c454e56737e79
yousejp <yousejp25@gmail.com>
2020-09-18 21:19:28 +0900

    webpackを使用しない場合のテスト

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import './style.scss';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './style.css';
+// import './style.scss';
 
-ReactDOM.render(
-  <div>Hello, React!</div>,
-  document.getElementById('root'),
-);
+// ReactDOM.render(
+//   <div>Hello, React!</div>,
+//   document.getElementById('root'),
+// );
+
+function component() {
+  const element = document.createElement('div');
+  const array = ['Hello', 'webpack'];
+  element.innerHTML = _.join(array, ' ');
+  return element;
+}
+
+document.body.appendChild(component());
