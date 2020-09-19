1f5dde37e685b719a017d63a0e47e0971a06a0b1
yousejp <yousejp25@gmail.com>
2020-09-19 09:13:11 +0900

    webpackの使い方を勉強

--- src/index.js
+++ src/index.js
@@ -8,10 +8,24 @@
 //   document.getElementById('root'),
 // );
 
+import _ from 'lodash';
+
+// テストコメント
+/* テストだよコメント
+ */
+
+/* 
+ @license
+ ライセンスコメント
+ */
+
 function component() {
   const element = document.createElement('div');
   const array = ['Hello', 'webpack'];
   element.innerHTML = _.join(array, ' ');
+  /* テスト2コメント
+   */
+  console.log('コンソール');
   return element;
 }
 
