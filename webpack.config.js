60973d8ea784a87c4d6f1e605b73a0d23665ac62
yousejp <yousejp25@gmail.com>
2020-09-21 07:55:00 +0900

    console.logの削除プラグインを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const outputPath = path.resolve(__dirname, 'dist');
 // console.log({ outputPath });
@@ -71,4 +72,15 @@ module.exports = {
     }),
     // [hash]はキャッシュにかからないために使用
   ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+  },
 };
